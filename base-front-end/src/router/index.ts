98ed38148ed8f703983d7da091908b0b7716e51a
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-05-13 11:27:06 -0300

    No http on dev and composition api + options

--- base-front-end/src/router/index.ts
+++ base-front-end/src/router/index.ts
@@ -16,6 +16,11 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/Test",
+    name: "Test",
+    component:() => import("../views/Test.vue")
+  }
 ];
 
 const router = createRouter({
---
+++ base-front-end/src/views/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>This is a test page</h1>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    alert("");
+  },
+  name: "Test",
+  methods: {
+    ola() {
+      alert("olá");
+    },
+  },
+  mounted() {
+    this.ola();
+  },
+});
+</script>
