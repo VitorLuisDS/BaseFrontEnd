4cd689f198127341c469aefb1892990834712978
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-05-13 11:42:16 -0300

    Typescript tests

--- base-front-end/src/views/Test.vue
+++ base-front-end/src/views/Test.vue
@@ -1,21 +1,31 @@
 <template>
   <h1>This is a test page</h1>
+  <h4>{{ label.text }}</h4>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-
+interface Hello {
+  text: string;
+}
 export default defineComponent({
   setup() {
-    alert("");
+    console.log("-setup-");
+  },
+  data() {
+    return {
+      label: {} as Hello,
+    };
   },
   name: "Test",
   methods: {
-    ola() {
-      alert("olá");
+    hello(text: string) {
+      setTimeout(() => {
+        this.label.text = text;
+      }, 1000);
     },
   },
   mounted() {
-    this.ola();
+    this.hello("Introduction to Vue 3 + Composition Api + Typescript");
   },
 });
 </script>
