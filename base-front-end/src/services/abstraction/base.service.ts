d9ea1b810ce63ff125c267f3a90bab26c1296e8e
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-06-24 11:07:36 -0300

    Auth tests

--- base-front-end/src/services/abstraction/base.service.ts
+++ base-front-end/src/services/abstraction/base.service.ts
@@ -1,23 +1,28 @@
 import axios from "axios";
-import { AuthRepository } from "@/repositories/security/AuthRepository";
+//import { AuthRepository } from "@/repositories/security/AuthRepository";
 
-const tokens = AuthRepository().getAuthTokens();
+//const tokens = AuthRepository().getAuthTokens();
 
 const instance = axios.create({
     headers: {
-        "Access-Control-Allow-Origin": "*",
-        Authorization: "Bearer " + tokens?.accessToken,
-        refreshtoken: tokens?.refreshToken
+        // "Access-Control-Allow-Origin": "*",
+        //Authorization: "Bearer " + tokens?.accessToken,
+        //refreshtoken: tokens?.refreshToken
     }
 });
 
 export default {
     async get(url: string) {
-        return await instance.get(url);
+        return await instance.get(url,
+            { withCredentials: true }
+        );
     },
 
     async post(url: string, payload?: any) {
-        return await instance.post(url, payload);
+        return await instance.post(url, payload, {
+            headers: { 'Content-Type': 'application/json', "Set-Cookie": "asdasd" },
+            withCredentials: true
+        });
     },
 
     async put(url: string, payload?: any) {
--- base-front-end/src/views/core/login/script.ts
+++ base-front-end/src/views/core/login/script.ts
@@ -1,6 +1,7 @@
 import { computed, defineComponent, ref } from "vue";
 import { AuthTokens } from "@/models/security/AuthTokens";
 import { AuthRepository } from "@/repositories/security/AuthRepository";
+import baseService from "@/services/abstraction/base.service";
 
 export default defineComponent({
     setup() {
@@ -16,16 +17,25 @@ export default defineComponent({
     },
     methods: {
         async login() {
-            console.log(this.token);
-            await this.setTokenAsync(new AuthTokens('ola', 'teste'));
-            console.log(this.token);
-            // const result = false;
-            // const response = await service.post('https://localhost:44382/api/authentication', { login: this.username, password: this.password });
+            // console.log(this.token);
+            // await this.setTokenAsync(new AuthTokens('ola', 'teste'));
+            // console.log(this.token);
+            let result = false;
+            const response = await baseService.post('https://localhost:44382/api/authentication', { login: this.username, password: this.password });
+            console.log(response);
 
-            // console.log(response);
+            const response2 = await baseService.get('https://localhost:44382/WeatherForecast');
+            console.log(response2);
 
-            // if (!result)
-            //     this.$router.push({ name: "NotAuthorized", params: { message: "User profile not authorized" } });
+            // const response3 = await baseService.post('https://localhost:44382/api/authentication/logout');
+            // console.log(response3);
+
+            // const response4 = await baseService.get('https://localhost:44382/WeatherForecast');
+            // console.log(response4);
+
+            result = true;
+            if (!result)
+                this.$router.push({ name: "NotAuthorized", params: { message: "User profile not authorized" } });
         }
     }
 })
