a6864b93b0ad01c663b393a7cee0e265857ff389
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-06-28 14:41:52 -0300

    Element Plus instalation

--- base-front-end/src/main.ts
+++ base-front-end/src/main.ts
@@ -3,5 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import { STORE } from "./store";
-
-createApp(App).use(STORE).use(router).mount("#app");
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import '@/style/global.sass';
+createApp(App).use(STORE).use(router).use(ElementPlus).mount("#app");
--- base-front-end/src/views/core/login/Index.vue
+++ base-front-end/src/views/core/login/Index.vue
@@ -1,34 +1,57 @@
 <template>
-  <div class="login-form">
-    <form ref="formLogin">
-      <div><h1>Login</h1></div>
-      <div>
-        <input type="text" placeholder="Username" v-model="username" />
-      </div>
-      <br />
-      <div>
-        <input type="password" placeholder="Password" v-model="password" />
-      </div>
-      <div>
-        <label>Stay Connected</label>
-        <input type="checkbox" v-model="stayConnected" />
-      </div>
-      <br />
-      <div>
-        <button type="submit" :disabled="loading" @click="loginAsync()">
-          Login
-        </button>
-        <button
-          type="button"
-          @click="renewAccessToken()"
-          v-if="token"
-          :disabled="loading"
-        >
-          Renew AccessToken
-        </button>
-      </div>
-    </form>
-  </div>
+  <el-container>
+    <el-main>
+      <el-row type="flex" justify="center">
+        <el-col :xl="6" :lg="6" :md="8" :sm="10" :xs="24">
+          <el-card shadow="always" :body-style="{ padding: '20px' }">
+            <template #header>
+              <span><b>Login</b></span>
+            </template>
+            <form ref="formLogin">
+              <el-row type="flex" justify="center">
+                <el-col :span="18" :xs="24">
+                  <el-input
+                    placeholder="Username"
+                    v-model="username"
+                    suffix-icon="el-icon-user"
+                  />
+                </el-col>
+              </el-row>
+              <el-row type="flex" justify="center">
+                <el-col :span="18" :xs="24">
+                  <el-input
+                    type="password"
+                    placeholder="Password"
+                    suffix-icon="el-icon-lock"
+                    v-model="password"
+                  />
+                </el-col>
+              </el-row>
+              <el-row type="flex" justify="center">
+                <el-col :span="18" :xs="24">
+                  <el-checkbox
+                    label="Stay Connected"
+                    type="checkbox"
+                    v-model="stayConnected"
+                  />
+                </el-col>
+              </el-row>
+              <el-row type="flex" justify="center">
+                <el-col :span="18" :xs="24">
+                  <el-button
+                    type="primary"
+                    :loading="loading"
+                    @click="loginAsync()"
+                    >Login</el-button
+                  >
+                </el-col>
+              </el-row>
+            </form>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 <script src="./script.ts" />
 <style lang="sass" scoped>
